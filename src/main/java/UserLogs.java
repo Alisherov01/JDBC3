d2e1402082ff7f4877e01c665f0ba6eae2f79d4f
erlan_alisherov <alisherovv7@bk.ru>
2023-02-23 17:36:47 +0600

    first commit

---
+++ src/main/java/UserLogs.java
@@ -0,0 +1,14 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLogs {
+    private Long userId;
+    private Timestamp dateOfAuthorization;
+    private Boolean success;
+}
---
+++ src/main/java/Users.java
@@ -0,0 +1,16 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+    private Long id;
+    private String login;
+    private String email;
+    private String password;
+    private Timestamp dateOfRegistration;
+}
